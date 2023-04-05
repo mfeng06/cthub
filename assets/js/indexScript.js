49d8c001e738d3ff72f886038dc2c064326c9500
mfeng06 <cutydragy@gmail.com>
2023-04-05 17:32:18 -0500

    blurlists

--- assets/js/indexScript.js
+++ assets/js/indexScript.js
@@ -1,75 +1,94 @@
-var fileInput = document.getElementById('myfile');
- var   info = document.getElementById('info');
+// var fileInput = document.getElementById('myfile');
+//  var   info = document.getElementById('info');
     // preview = document.getElementById('blur');
-    const preview = document.querySelector('.preview1');
-    const clearPreview = document.querySelector('.preview2');
+    var preview = document.querySelector('.preview1');
+    var clearPreview = document.querySelector('.preview2');
     var imageblur = document.getElementById('blur-image');
     var trans = document.getElementById('transfer');
     var clearname = "";
     var download = document.getElementById('download');
+    var getBlurImg = document.getElementById('selectblurimg');
+
+    var blurLists = ["","blur1","blur2","blur3"];
 
 
 window.addEventListener("load",initialization,false)
 
 function initialization() {
+  createblurList();
 
-    fileInput.addEventListener('change', function () {
+    // fileInput.addEventListener('change', function () {
     
-        while(preview.firstChild) {
-            preview.removeChild(preview.firstChild);
-          }
+    //     while(preview.firstChild) {
+    //         preview.removeChild(preview.firstChild);
+    //       }
         
-          const curFiles = fileInput.files;
-          if (curFiles.length === 0) {
-            info.innerHTML = 'No files currently selected for upload';
-          } else {
-            var para = "";
-            const file = curFiles[0];
-            if (validFileType(file)) {
-              para = `File name: ${file.name}<br> File size: ${returnFileSize(file.size)}.`;
-              const image = document.createElement('img');
-              image.src = URL.createObjectURL(file);
-              image.id = "blur";
-              preview.appendChild(image);
+    //       const curFiles = fileInput.files;
+    //       if (curFiles.length === 0) {
+    //         info.innerHTML = 'No files currently selected for upload';
+    //       } else {
+    //         var para = "";
+    //         const file = curFiles[0];
+    //         if (validFileType(file)) {
+    //           para = `File name: ${file.name}<br> File size: ${returnFileSize(file.size)}.`;
+    //           const image = document.createElement('img');
+    //           image.src = URL.createObjectURL(file);
+    //           image.id = "blur";
+    //           preview.appendChild(image);
               
-              info.innerHTML = para;
-              trans.style.pointerEvents = "auto";
-              download.style.pointerEvents="none";
-            } else {
-              para = `File name: ${file.name} not a valid file type`;
-             info.innerHTML=para;
-            }
+    //           info.innerHTML = para;
+    //           trans.style.pointerEvents = "auto";
+    //           download.style.pointerEvents="none";
+    //         } else {
+    //           para = `File name: ${file.name} not a valid file type`;
+    //          info.innerHTML=para;
+    //         }
       
-          }
-        })
+    //       }
+    //     })
 
         trans.addEventListener('click',function(){
+          var nodeblur = true;
             while(clearPreview.firstChild) {
                 clearPreview.removeChild(clearPreview.firstChild);
               }
-              const curFiles = fileInput.files;
+              // const curFiles = fileInput.files;
             var image = document.createElement('img');
-            if(curFiles[0].name =="blur1.png")
-              {
-                // alert("one");
-                image.src = "assets/img/deblur1.png";
-                clearname = "deblur1";
+            // if(curFiles[0].name =="blur1.png")
+            for(var i=1;i<blurLists.length;i++){
+              if(getBlurImg.value == blurLists[i]){
+                image.src = "assets/img/" + "de" + blurLists[i] +".png";
+                clearname = "de" + blurLists[i];
                 download.style.pointerEvents="auto";
+                nodeblur = false;
               }
-                else if(curFiles[0].name =="blur2.png"){
-                  image.src = "assets/img/deblur2.png";
-                  clearname = "deblur2";
-                  download.style.pointerEvents="auto";
-                }
-                else if(curFiles[0].name =="blur3.png"){
-                  image.src = "assets/img/deblur3.png";
-                  clearname = "deblur3";
-                  download.style.pointerEvents="auto";
-                }
-                else{
-                  image.src = "assets/img/no.png";
-                  download.style.pointerEvents="none";
-                }
+            }
+            if(nodeblur){
+              image.src = "assets/img/no.png";
+              download.style.pointerEvents="none";
+            }
+
+            // if(getBlurImg.value == "blur1")
+            //   {
+            //     // alert("one");
+            //     image.src = "assets/img/deblur1.png";
+            //     clearname = "deblur1";
+            //     download.style.pointerEvents="auto";
+            //   }
+            //     else if(getBlurImg.value =="blur2"){
+            //       image.src = "assets/img/deblur2.png";
+            //       clearname = "deblur2";
+            //       download.style.pointerEvents="auto";
+            //     }
+            //     else if(getBlurImg.value =="blur3"){
+            //       image.src = "assets/img/deblur3.png";
+            //       clearname = "deblur3";
+            //       download.style.pointerEvents="auto";
+            //     }
+            //     else{
+            //       image.src = "assets/img/no.png";
+            //       download.style.pointerEvents="none";
+            //     }
               image.id = "clear";
               
 
@@ -77,7 +96,7 @@ function initialization() {
               // word.textContent = "clear";
               clearPreview.appendChild(image);
               // clearPreview.appendChild(word);
-              trans.style.pointerEvents = "none";
+              // trans.style.pointerEvents = "none";
         })
 
         // 添加download功能
@@ -122,6 +141,23 @@ const fileTypes = [
     }
   }
   
-
+  function createblurList(){
+    for(var i=0;i<blurLists.length;i++){       
+      var item = document.createElement('option')
+      item.className = "blurLists";
+      item.value = blurLists[i];
+      item.innerHTML = blurLists[i];
+      // item.addEventListener('change',blurClicked)
+      getBlurImg.appendChild(item);
+  }
+  }
   
-  
+  function blurClicked(){
+    while(preview.firstChild) {
+      preview.removeChild(preview.firstChild);
+    }
+    const image = document.createElement('img');
+    image.src = "assets/img/blur/" + getBlurImg.value + ".png";
+    image.id = "blur";
+    preview.appendChild(image);
+  }
